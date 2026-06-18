15cd8b5c8324ce824fd0fcf8a7d8acbad097dbbc
ch1nazec <zaicev.casha10@gmail.com>
2026-06-19 02:20:27 +1000

    end the 1 lesson

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from typing import Optional
 
 
 app = FastAPI()
@@ -14,4 +15,44 @@ async def contacts() -> int:
     return 1000
 
 
-posts = 
+posts = [
+    {
+        'id': 1,
+        'title': 'News 1',
+        'body': 'Text 1'
+    },
+    {
+        'id': 2,
+        'title': 'News 2',
+        'body': 'Text 2'
+    },
+    {
+        'id': 3,
+        'title': 'News 3',
+        'body': 'Text 3'
+    },
+]
+
+
+@app.get('/items')
+async def items() -> list[dict]:
+    return posts
+
+
+@app.get('/items/{id}')
+async def items(id: int) -> dict:
+    for post in posts:
+        if post.get('id') == id:
+            return post
+    raise HTTPException(status_code=404, detail='Post not found')
+
+
+@app.get('/search')
+async def search(post_id: Optional[int] = None) -> dict:
+    if post_id:
+        for post in posts:
+            if post.get('id') == post_id:
+                return post
+        raise HTTPException(status_code=404, detail='Post not found')
+    else:
+        return {'data': 'No post id provided'}
