63067712a6e3297c6ed9515adc92dfcd21554705
ch1nazec <zaicev.casha10@gmail.com>
2026-06-19 02:40:21 +1000

    pydantic ends

--- main.py
+++ main.py
@@ -5,52 +5,80 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+
 
 class Post(BaseModel):
     id: int
     title: str
     body: str
+    author: User
 
+
+users = [
+    {
+        'id': 1,
+        'name': 'John',
+        'age': 30
+    },
+    {
+        'id': 2,
+        'name': 'Danny',
+        'age': 25
+    },
+    {
+        'id': 3,
+        'name': 'Pididid',
+        'age': 18
+    },
+]
+users = [User(**item) for item in users]
 
 posts = [
     {
         'id': 1,
         'title': 'News 1',
-        'body': 'Text 1'
+        'body': 'Text 1',
+        'author': users[0]
     },
     {
         'id': 2,
         'title': 'News 2',
-        'body': 'Text 2'
+        'body': 'Text 2',
+        'author': users[1]
     },
     {
         'id': 3,
         'title': 'News 3',
-        'body': 'Text 3'
+        'body': 'Text 3',
+        'author': users[2]
     },
 ]
+posts = [Post(**item) for item in posts]
 
 
 @app.get('/items')
 async def items() -> list[Post]:
-    data_posts = [Post(**item) for item in posts]
-    return data_posts
+    return posts
 
 
 @app.get('/items/{id}')
-async def items(id: int) -> dict:
+async def items(id: int) -> Post:
     for post in posts:
-        if post.get('id') == id:
+        if post.id == id:
             return post
     raise HTTPException(status_code=404, detail='Post not found')
 
 
 @app.get('/search')
-async def search(post_id: Optional[int] = None) -> dict:
+async def search(post_id: Optional[int] = None) -> dict[str, Optional[Post]]:
     if post_id:
         for post in posts:
-            if post.get('id') == post_id:
-                return post
+            if post.id == post_id:
+                return {'data': post}
         raise HTTPException(status_code=404, detail='Post not found')
     else:
-        return {'data': 'No post id provided'}
+        return {'data': None}
